38ded7ceeeb10d794fe70919cd3a2cc7b78457d3
Satyam <satyam@pacdevelopers.com>
2021-10-12 08:23:09 +0530

    Changes

--- iOSAssessment-11/iOSAssessment-11/AutolayoutProgramaticVc.swift
+++ iOSAssessment-11/iOSAssessment-11/AutolayoutProgramaticVc.swift
@@ -21,11 +21,17 @@ class AutolayoutProgramaticVc: UIViewController {
     @IBOutlet weak var lbl3: UILabel!
     @IBOutlet weak var lbl4: UILabel!
     
-    //IBOutlets for stackViews
-    @IBOutlet weak var mainStackView: UIStackView!
-    @IBOutlet weak var firstVerticalStackView: UIStackView!
-    @IBOutlet weak var secondVerticalStackView: UIStackView!
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var view3: UIView!
+    @IBOutlet weak var view4: UIView!
     
+    //    //IBOutlets for stackViews
+    
+    //    @IBOutlet weak var mainStackView: UIStackView!
+//    @IBOutlet weak var firstVerticalStackView: UIStackView!
+//    @IBOutlet weak var secondVerticalStackView: UIStackView!
+//
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -39,28 +45,41 @@ class AutolayoutProgramaticVc: UIViewController {
         lbl4.translatesAutoresizingMaskIntoConstraints = false
        
         
-        let img1TopContraint = img1.topAnchor.constraint(equalTo: mainStackView.topAnchor, constant: 10)
-        let img1LeadingContraint = img1.leadingAnchor.constraint(equalTo: mainStackView.leadingAnchor, constant: 10)
-        let img1WidthContraint = img1.widthAnchor.constraint(equalToConstant: 170)
-        let img1HeightContraint = img1.heightAnchor.constraint(equalToConstant: 300)
-        
-        NSLayoutConstraint.activate([img1TopContraint, img1LeadingContraint, img1WidthContraint, img1HeightContraint])
-        
-        
-        
-        let img2TopContraint = img2.topAnchor.constraint(equalTo: mainStackView.topAnchor, constant: 20)
-        let img2TrailingContraint = img2.trailingAnchor.constraint(equalTo: mainStackView.trailingAnchor)
-        let img2WidthContraint = img2.widthAnchor.constraint(equalToConstant: 200)
-        let img2HeightContraint = img2.heightAnchor.constraint(equalToConstant: 170)
-        
-        NSLayoutConstraint.activate([img2TopContraint, img2TrailingContraint, img2WidthContraint, img2HeightContraint])
+        let img1TopContraint = img1.topAnchor.constraint(equalTo: view1.topAnchor, constant: 10)
+        let img1LeadingContraint = img1.leadingAnchor.constraint(equalTo: view1.leadingAnchor, constant: 10)
+        let img1BottomContraint = img1.bottomAnchor.constraint(equalTo: view1.bottomAnchor, constant: 30)
+        let img1TrailingContraint = img1.trailingAnchor.constraint(equalTo: view1.trailingAnchor, constant: 10)
+//        let img1WidthContraint = img1.widthAnchor.constraint(equalToConstant: 170)
+//        let img1HeightContraint = img1.heightAnchor.constraint(equalToConstant: 300)
+
+        NSLayoutConstraint.activate([img1TopContraint, img1LeadingContraint, img1BottomContraint, img1TrailingContraint])
         
         
-        let lbl1TopContraint = lbl1.topAnchor.constraint(equalTo: img1.bottomAnchor, constant: 10)
-        let lbl1LeadingContraint = lbl1.leadingAnchor.constraint(equalTo: mainStackView.leadingAnchor, constant: 20)
-        let lbl1WidthContraint = lbl1.widthAnchor.constraint(equalToConstant: 170)
         
-        NSLayoutConstraint.activate([lbl1TopContraint, lbl1LeadingContraint, lbl1WidthContraint])
+        let img2TopContraint = img2.topAnchor.constraint(equalTo: view2.topAnchor, constant: 10)
+        let img2LeadingContraint = img2.leadingAnchor.constraint(equalTo: view2.leadingAnchor, constant: 10)
+        let img2BottomContraint = img2.bottomAnchor.constraint(equalTo: view2.bottomAnchor, constant: 30)
+        let img2TrailingContraint = img2.trailingAnchor.constraint(equalTo: view2.trailingAnchor, constant: 10)
+        
+        NSLayoutConstraint.activate([img2TopContraint, img2TrailingContraint, img2BottomContraint, img2LeadingContraint])
+
+        
+        let img3TopContraint = img3.topAnchor.constraint(equalTo: view3.topAnchor, constant: 10)
+        let img3LeadingContraint = img3.leadingAnchor.constraint(equalTo: view3.leadingAnchor, constant: 10)
+        let img3BottomContraint = img3.bottomAnchor.constraint(equalTo: view3.bottomAnchor, constant: 30)
+        let img3TrailingContraint = img3.trailingAnchor.constraint(equalTo: view3.trailingAnchor, constant: 10)
+        
+        NSLayoutConstraint.activate([img3TopContraint, img3TrailingContraint, img3BottomContraint, img3LeadingContraint])
+        
+        
+        
+        let img4TopContraint = img4.topAnchor.constraint(equalTo: view4.topAnchor, constant: 10)
+        let img4LeadingContraint = img4.leadingAnchor.constraint(equalTo: view2.leadingAnchor, constant: 10)
+        let img4BottomContraint = img4.bottomAnchor.constraint(equalTo: view4.bottomAnchor, constant: 30)
+        let img4TrailingContraint = img4.trailingAnchor.constraint(equalTo: view4.trailingAnchor, constant: 10)
+        
+        NSLayoutConstraint.activate([img4TopContraint, img4TrailingContraint, img4BottomContraint, img4LeadingContraint])
+
     }
     
 }
--- iOSAssessment-11/iOSAssessment-11/StackViewDemoVC.swift
+++ iOSAssessment-11/iOSAssessment-11/StackViewDemoVC.swift
@@ -9,21 +9,57 @@ import UIKit
 
 class StackViewDemoVC: UIViewController {
 
+    var current : UIView?
+    
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var view3: UIStackView!
+    @IBOutlet weak var view2: UIStackView!
+    @IBOutlet weak var view4: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view1.isHidden = true
+        view2.isHidden = true
+        view3.isHidden = true
+        view4.isHidden = true
+        
+    }
+
+
+    func showAlert() {
+        let alert = UIAlertController(title: "Alert", message: "Enter number from 1 to 4 only", preferredStyle: .alert)
+        self.present(alert, animated: true, completion: nil)
+        Timer.scheduledTimer(withTimeInterval: 2.0, repeats: false, block: { _ in alert.dismiss(animated: true, completion: nil)} )
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func showView(_ sender: UIButton) {
+        
+        let count = input.text
+        if current != nil{
+            current?.isHidden = true
+        }
+      
+        switch(count){
+        case "1":
+            view1.isHidden = false
+            current = view1
+        case "2":
+            view2.isHidden = false
+            current = view2
+        case "3":
+            view3.isHidden = false
+            current = view3
+        case "4":
+            view4.isHidden = false
+            current = view4
+        default:
+            showAlert()
+        }
+        
     }
-    */
-
+    
+    
 }
