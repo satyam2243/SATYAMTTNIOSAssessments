e7c0b27f1fafbab7f88b339130fd6897d51169e2
Satyam <satyam@pacdevelopers.com>
2021-11-12 16:44:15 +0530

    Changes

--- iOSAssessment-20/iOSAssessment-20/MapViewController.swift
+++ iOSAssessment-20/iOSAssessment-20/MapViewController.swift
@@ -11,154 +11,114 @@ import CoreLocation
 
 class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate  {
 
-    var locationManager: CLLocationManager?
-
-      var currentLocation: MKUserLocation? {
-          didSet {
-              button.isHidden = (currentLocation == nil)
-          }
-      }
-
-    lazy var formatter: DateFormatter = {
-
-        let formatter = DateFormatter()
-        formatter.timeStyle = DateFormatter.Style.short
-        formatter.dateStyle = DateFormatter.Style.short
-          return formatter
-      }()
-
-      @IBOutlet var button: UIButton!
-      @IBOutlet var mapView: MKMapView!
-
-      override func viewDidLoad() {
-
-          super.viewDidLoad()
-          
-          //Deadlock Example --->>
-
-//          DispatchQueue.main.sync {
-//              DispatchQueue.main.sync {
-//                  //
-//              }
-//          }
-
-          if CLLocationManager.locationServicesEnabled() {
-
-              locationManager = CLLocationManager()
-              locationManager?.delegate = self
-              locationManager?.desiredAccuracy = kCLLocationAccuracyBest
-
-              switch CLLocationManager.authorizationStatus() {
-
-              case .authorizedAlways, .authorizedWhenInUse:
-                  locationManager?.startUpdatingLocation()
-                  mapView.showsUserLocation = true
-
-              default:
-                  locationManager?.requestWhenInUseAuthorization()
-              }
-          }
-      }
-
-    private func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
-
-          switch status {
-
-          case .authorizedAlways, .authorizedWhenInUse:
-              locationManager?.startUpdatingLocation()
-              mapView.showsUserLocation = true
-
-          case .denied, .restricted:
-              locationManager?.startUpdatingLocation()
-              mapView.showsUserLocation = false
-              currentLocation = nil
-
-          default:
-              currentLocation = nil
-          }
-      }
-
-    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-
-          currentLocation = userLocation
-      }
-
-      @IBAction func addButtonTapped(sender: AnyObject) {
-
-          guard let coordinate = currentLocation?.coordinate else {
-              return
-          }
-
-          let reportTime = Date()
-          let formattedTime = formatter.string(from: reportTime)
-
-          let annotation = MKPointAnnotation()
-          annotation.title = "Annotation Created"
-          annotation.subtitle = formattedTime
-          annotation.coordinate = coordinate
-
-          mapView.addAnnotation(annotation)
-      }
-
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-
-          guard !annotation.isKind(of: MKUserLocation.self) else {
-
-              return nil
-          }
-
-          let annotationIdentifier = "AnnotationIdentifier"
-
-          var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier)
-
-          if annotationView == nil {
-              annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
-              annotationView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-              annotationView!.canShowCallout = true
-          }
-          else {
-              annotationView!.annotation = annotation
-          }
-
-          annotationView!.image = UIImage(named: "smile")
-
-          return annotationView
-
-      }
+    @IBOutlet weak var mapView: MKMapView!
     
-//    let coords = [  CLLocation(latitude: 28.7041, longitude: 77.1025),
-//       CLLocation(latitude: 27.1767, longitude:  78.0081),
-//       CLLocation(latitude: 27.4757, longitude: 77.6937)
-//        ];
-//
-//
-//    func addAnnotations(coords: [CLLocation]){
-//            for coord in coords{
-//                let CLLCoordType = CLLocationCoordinate2D(latitude: coord.coordinate.latitude,
-//                                                          longitude: coord.coordinate.longitude);
-//                let anno = MKPointAnnotation();
-//                anno.coordinate = CLLCoordType;
-//                mapView.addAnnotation(anno);
-//            }
-//
-//        }
-//
-//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-//        if annotation is MKUserLocation{
-//            return nil;
-//        }else{
-//            let pinIdent = "Pin";
-//            var pinView: MKPinAnnotationView;
-//            if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: pinIdent) as? MKPinAnnotationView {
-//                dequeuedView.annotation = annotation;
-//                pinView = dequeuedView;
-//            }else{
-//                pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: pinIdent);
-//
-//            }
-//            return pinView;
-//        }
-//    }
+    let locationManager = CLLocationManager()
+    var currentCoordinate: CLLocationCoordinate2D!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+        checkLocationServices()
+    }
+        
+    func setLocationManager()
+    {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+            
+    func checkLocationServices()
+    {
+        if CLLocationManager.locationServicesEnabled()
+        {
+            setLocationManager()
+            checkLocationAuthorization()
+            addAnnotationsToMap()
+        }
+        else
+        {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+        
+        
+    func checkLocationAuthorization()
+    {
+        switch  locationManager.authorizationStatus{
+        case .authorizedWhenInUse:
+            beginLocationUpdates(locationManager: locationManager)
+        case .denied:
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            break
+        case .authorizedAlways:
+            beginLocationUpdates(locationManager: locationManager)
+        @unknown default:
+            fatalError()
+        }
+    }
+        
+    func addAnnotationsToMap()
+    {
+        let annotation1 = MKPointAnnotation()
+        annotation1.title = "Anno_1"
+        annotation1.coordinate = CLLocationCoordinate2D(latitude: 19.089403, longitude: 72.890428)
+            
+        let annotation2 = MKPointAnnotation()
+        annotation2.title = "Anno_2"
+        annotation2.coordinate = CLLocationCoordinate2D(latitude: 19.064789, longitude:72.902401)
+        
+        let annotation3 = MKPointAnnotation()
+        annotation2.title = "Anno_3"
+        annotation2.coordinate = CLLocationCoordinate2D(latitude: 19.018710, longitude:72.850672)
+        
+        let annotation4 = MKPointAnnotation()
+        annotation2.title = "Anno_4"
+        annotation2.coordinate = CLLocationCoordinate2D(latitude:  19.069480, longitude:72.842799)
+        
+        let annotation5 = MKPointAnnotation()
+        annotation2.title = "Anno_5"
+        annotation2.coordinate = CLLocationCoordinate2D(latitude: 19.069476, longitude:72.842797)
+        
+        let arr = [annotation1,annotation2,annotation3,annotation4,annotation5]
+        
+        mapView.addAnnotations(arr)
 
-  }
+    }
+    func beginLocationUpdates(locationManager:CLLocationManager){
+        mapView.showsUserLocation = true
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+    
+    func zoomToLatestLocation(with coordinate: CLLocationCoordinate2D){
+        let span = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+
+    }
+}
+
+
+extension MapViewController{
+    func locationManager(_ manager : CLLocationManager, didUpdateLocations locations:[CLLocation])
+    {
+        guard let latestLocation = locations.first else {
+            return
+        }
+        if currentCoordinate == nil{
+            zoomToLatestLocation(with: latestLocation.coordinate)
+        }
+        currentCoordinate = latestLocation.coordinate
+        
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager, didChangeAuthorization status : CLAuthorizationStatus) {
+            
+    }
+}
+
 
