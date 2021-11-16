651d6e68ec3e65e5312ae1f8909ab6da683fb7f1
Satyam <satyam@pacdevelopers.com>
2021-11-16 21:19:42 +0530

    Changes

--- iOSAssessment-23/iOSAssessment-23/Controller/RecipiesDetailsViewController.swift
+++ iOSAssessment-23/iOSAssessment-23/Controller/RecipiesDetailsViewController.swift
@@ -28,108 +28,147 @@ class RecipiesDetailsViewController: UIViewController {
     
     //MARK: - Actions
     
+    @IBAction func addRecipiesinDB(_ sender: Any) {
+        
+//        guard let textFieldSSN = alert.textFields?[1],
+//          let SSNToSave = textFieldSSN.text else {
+//            return
+//        }
+//
+//        self.save(name: nameToSave, ssn: Int16(SSNToSave)!)
+        
+        guard let nametextField = nameTf, let nameToSAve = nametextField.text else {
+            return
+        }
+        
+        guard let ingrediantestextField = ingrediantesTf, let ingrediantesToSAve = ingrediantestextField.text else {
+            return
+        }
+        
+        guard let datetextField = dateTf, let dateToSAve = datetextField.text else {
+            return
+        }
+        
+        guard let createdBytextField = createdByTf, let createdByToSAve = createdBytextField.text else {
+            return
+        }
+        
+        guard let timeToPreparetextField = timeToPrepareTf, let timeToPrepareToSAve = timeToPreparetextField.text else {
+            return
+        }
+        
+        self.save(name: nameToSAve, ingredients: <#T##String?#>, date: <#T##Date?#>, createdBy: <#T##String?#>, timeToPrepare: <#T##Float#>)
+        
+    }
     
     //MARK: - Methods
-
-    func createData(){
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
-        
-        let managedContext = appDelegate.persistentContainer.viewContext
-        
-        let RecipeEntity = NSEntityDescription.entity(forEntityName: "Recipe", in: managedContext)!
-        
-        let recipe = NSManagedObject(entity: RecipeEntity, insertInto: managedContext)
-        recipe.setValue(nameTf.text, forKey: "name")
-        recipe.setValue(dateTf.text, forKey: "date")
-        recipe.setValue(ingrediantesTf.text, forKey: "ingredients")
-        recipe.setValue(createdByTf.text, forKey: "createdBy")
-        recipe.setValue(timeToPrepareTf.text, forKey: "timeToPrepare")
-        
-        do {
-            try managedContext.save()
-        } catch let err as NSError {
-            print("Could not save. \(err). \(err.userInfo)")
-        }
-        
-    }
     
-    func retrieveData() {
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
-        
-        let managedContext = appDelegate.persistentContainer.viewContext
-        
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Recipe")
-        
-        do {
-            let result = try managedContext.fetch(fetchRequest)
-            
-            for data in result as! [NSManagedObject] {
-                print(data.value(forKey: "name") as! String)
-                
-                
-                
-            }
-        }   catch {
-            print("Failed")
-        }
-    }
+    //insert
+    func save(name: String?, ingredients : String?, date : Date?, createdBy : String?, timeToPrepare : Float) {
+      
+        let _ = CoreDataManager.sharedManager.insertRecipe(name: name, ingredients: ingredients, date: date, createdBy: createdBy, timeToPrepare: timeToPrepare)
+  }
 
-    func updateData() {
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
-        
-        let managedContext = appDelegate.persistentContainer.viewContext
-        
-        let fetchRequest:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "Recipe")
-        fetchRequest.predicate = NSPredicate(format: "name = %@", "RejmaAndChawal")
-        
-        do
-        {
-            let test = try managedContext.fetch(fetchRequest)
-            
-            let objectUpdate = test[0] as! NSManagedObject
-            objectUpdate.setValue("newName", forKey: "name")
-            objectUpdate.setValue("newDate", forKey: "date")
-            objectUpdate.setValue("newIngredients", forKey: "ingredients")
-            objectUpdate.setValue("newCreatedBy", forKey: "createdBy")
-            
-            do {
-                try managedContext.save()
-            }
-            catch {
-                print(error)
-            }
-        }
-        
-        catch {
-            print(error)
-        }
-    }
+//    func createData(){
+//        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+//
+//        let managedContext = appDelegate.persistentContainer.viewContext
+//
+//        let RecipeEntity = NSEntityDescription.entity(forEntityName: "Recipe", in: managedContext)!
+//
+//        let recipe = NSManagedObject(entity: RecipeEntity, insertInto: managedContext)
+//        recipe.setValue(nameTf.text, forKey: "name")
+//        recipe.setValue(dateTf.text, forKey: "date")
+//        recipe.setValue(ingrediantesTf.text, forKey: "ingredients")
+//        recipe.setValue(createdByTf.text, forKey: "createdBy")
+//        recipe.setValue(timeToPrepareTf.text, forKey: "timeToPrepare")
+//
+//        do {
+//            try managedContext.save()
+//        } catch let err as NSError {
+//            print("Could not save. \(err). \(err.userInfo)")
+//        }
+//
+//    }
     
-    func deleteData() {
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
-        
-        let managedContext = appDelegate.persistentContainer.viewContext
-        let fetchRequest:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "Recipe")
-        fetchRequest.predicate = NSPredicate(format: "name = %@", "RejmaAndChawal")
-        
-        do {
-            let test = try managedContext.fetch(fetchRequest)
-            
-            let objectToDelete = test[0] as! NSManagedObject
-            managedContext.delete(objectToDelete)
-            
-            do{
-                try managedContext.save()
-            }
-            catch
-            {
-                print(error)
-            }
-        }
-        catch
-        {
-            print(error)
-        }
+//    func retrieveData() {
+//        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+//
+//        let managedContext = appDelegate.persistentContainer.viewContext
+//
+//        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Recipe")
+//
+//        do {
+//            let result = try managedContext.fetch(fetchRequest)
+//
+//            for data in result as! [NSManagedObject] {
+//                print(data.value(forKey: "name") as! String)
+//
+//
+//
+//            }
+//        }   catch {
+//            print("Failed")
+//        }
+//    }
 
-    }
+//    func updateData() {
+//        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+//
+//        let managedContext = appDelegate.persistentContainer.viewContext
+//
+//        let fetchRequest:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "Recipe")
+//        fetchRequest.predicate = NSPredicate(format: "name = %@", "RejmaAndChawal")
+//
+//        do
+//        {
+//            let test = try managedContext.fetch(fetchRequest)
+//
+//            let objectUpdate = test[0] as! NSManagedObject
+//            objectUpdate.setValue("newName", forKey: "name")
+//            objectUpdate.setValue("newDate", forKey: "date")
+//            objectUpdate.setValue("newIngredients", forKey: "ingredients")
+//            objectUpdate.setValue("newCreatedBy", forKey: "createdBy")
+//
+//            do {
+//                try managedContext.save()
+//            }
+//            catch {
+//                print(error)
+//            }
+//        }
+//
+//        catch {
+//            print(error)
+//        }
+//    }
+    
+//    func deleteData() {
+//        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+//
+//        let managedContext = appDelegate.persistentContainer.viewContext
+//        let fetchRequest:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "Recipe")
+//        fetchRequest.predicate = NSPredicate(format: "name = %@", "RejmaAndChawal")
+//
+//        do {
+//            let test = try managedContext.fetch(fetchRequest)
+//
+//            let objectToDelete = test[0] as! NSManagedObject
+//            managedContext.delete(objectToDelete)
+//
+//            do{
+//                try managedContext.save()
+//            }
+//            catch
+//            {
+//                print(error)
+//            }
+//        }
+//        catch
+//        {
+//            print(error)
+//        }
+//
+//    }
+    
 }
--- iOSAssessment-23/iOSAssessment-23/CoreDataManager.swift
+++ iOSAssessment-23/iOSAssessment-23/CoreDataManager.swift
@@ -16,28 +16,138 @@ class CoreDataManager {
     
     
     lazy var persistentContainer: NSPersistentContainer = {
-      
-      let container = NSPersistentContainer(name: "iOSAssessment_23")
         
-      container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        let container = NSPersistentContainer(name: "iOSAssessment_23")
         
-        if let error = error as NSError? {
-          fatalError("Unresolved error \(error), \(error.userInfo)")
-        }
-      })
-      return container
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
     }()
     
+    
+    
+    
+    
+    
     func saveContext () {
-      let context = CoreDataManager.sharedManager.persistentContainer.viewContext
-      if context.hasChanges {
-        do {
-          try context.save()
-        } catch {
-          let nserror = error as NSError
-          fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        let context = CoreDataManager.sharedManager.persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
         }
+    }
+    
+    
+ 
+    
+    
+    func insertRecipe(name: String?, ingredients : String?, date : Date?, createdBy : String?, timeToPrepare : Float)->Recipe? {
+        
+        let managedContext = CoreDataManager.sharedManager.persistentContainer.viewContext
+        
+        let recipe = Recipe(context: managedContext)
+        
+        recipe.setValue(name, forKeyPath: "name")
+        recipe.setValue(ingredients, forKeyPath: "ingredients")
+        recipe.setValue(date, forKeyPath: "date")
+        recipe.setValue(createdBy, forKeyPath: "createdBy")
+        recipe.setValue(timeToPrepare, forKeyPath: "timeToPrepare")
+        
+        
+        do {
+            try managedContext.save()
+            return recipe as? Recipe
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+            return nil
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    func update(name: String?, ingredients : String?, date : Date?, createdBy : String?, timeToPrepare : Float, recipe : Recipe) {
+        
+        let context = CoreDataManager.sharedManager.persistentContainer.viewContext
+        
+        do {
+            
+            recipe.setValue(name, forKeyPath: "name")
+            recipe.setValue(ingredients, forKeyPath: "ingredients")
+            recipe.setValue(date, forKeyPath: "date")
+            recipe.setValue(createdBy, forKeyPath: "createdBy")
+            recipe.setValue(timeToPrepare, forKeyPath: "timeToPrepare")
+            
+            print("\(recipe.value(forKey: "name") ?? "")")
+            print("\(recipe.value(forKey: "createdBy") ?? "")")
+            
+            
+            do {
+                try context.save()
+                print("saved!")
+            } catch let error as NSError  {
+                print("Could not save \(error), \(error.userInfo)")
+            } catch {
+                
+            }
+            
+        } catch {
+            print("Error with request: \(error)")
+        }
+
+    }
+    
+    
+    
+    /*delete*/
+    func delete(recipe : Recipe){
+      
+      let managedContext = CoreDataManager.sharedManager.persistentContainer.viewContext
+      
+      do {
+        
+        managedContext.delete(recipe)
+        
+      } catch {
+        // Do something in response to error condition
+        print(error)
+      }
+      
+      do {
+        try managedContext.save()
+      } catch {
+        // Do something in response to error condition
       }
     }
+    
+    
+    
+    func fetchAllRecipies() -> [Recipe]?{
+      
+      let managedContext = CoreDataManager.sharedManager.persistentContainer.viewContext
+
+      let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+      
+      do {
+        let recipe = try managedContext.fetch(fetchRequest)
+        return recipe as? [Recipe]
+      } catch let error as NSError {
+        print("Could not fetch. \(error), \(error.userInfo)")
+        return nil
+      }
+    }
+    
+    
     
 }
